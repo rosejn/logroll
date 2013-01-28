4fd7f15ca6ea6cf93793afa21348cf0603a50496
Jeff Rose <jeff@deepmind.com>
2013-01-28 11:55:22 +0000

    cleaning up

--- logroll/init.lua
+++ logroll/init.lua
@@ -8,11 +8,12 @@ require 'fn'
 logroll = {}
 
 local DEFAULT_LEVEL = 'INFO'
-local level_strs = {'DEBUG', 'INFO', 'WARN', 'ERROR'}
+local LOG_LEVELS = {'DEBUG', 'INFO', 'WARN', 'ERROR'}
 
-for i, label in ipairs(level_strs) do
+for i, label in ipairs(LOG_LEVELS) do
     logroll[label] = i
 end
+
 
 local function default_formatter(level, ...)
     local msg = nil
@@ -23,14 +24,16 @@ local function default_formatter(level, ...)
         msg = ({...})[1]
     end
 
-    return string.format("[%s - %s] - %s\n", level_strs[level], os.date("%Y_%m_%d_%X"), msg)
+    return string.format("[%s - %s] - %s\n", LOG_LEVELS[level], os.date("%Y_%m_%d_%X"), msg)
 end
+
 
 local function default_writer(logger, level, ...)
     if level >= logger.level then
         logger.file:write(logger.formatter(level, unpack({...})))
     end
 end
+
 
 local function make_logger(file, options)
     local logger = {options   = options,
@@ -44,14 +47,16 @@ local function make_logger(file, options)
         lg[string.lower(level)] = fn.partial(logger.writer, logger, logroll[level])
         return lg
     end,
-    logger, level_strs)
+    logger, LOG_LEVELS)
 end
+
 
 -- A simple logger to print to STDIO.
 function logroll.print_logger(options)
     local options = options or {}
     return make_logger(io.stdout, options)
 end
+
 
 -- A logger that prints to a file.
 function logroll.file_logger(path, options)
